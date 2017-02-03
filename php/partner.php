ef7d2c55acc7738110b7202ad6120d016f7383dc
shadaxv <aleksandern1116@gmail.com>
2017-02-03 16:04:07 +0100

    Add files via upload

--- php/partner.php
+++ php/partner.php
@@ -56,43 +56,7 @@
 
 				<header>
 
-					<div class="account-nav">
-
-							<div class="header-left">
-								<div class="social-btn">
-									<div style="float:left; padding: 2px;"><a href="http://facebook.com" target="_blank"><i class="icon-facebook"></i></a></div>
-									<div style="float:left; padding: 2px;"><a href="http://twitter.com" target="_blank"><i class="icon-twitter"></i></a></div>
-									<div style="float:left; padding: 2px;"><a href="http://plus.google.com" target="_blank"><i class="icon-gplus"></i></a></div>
-									<div style="float:left; padding: 2px;"><a href="http://linkedin.com" target="_blank"><i class="icon-linkedin"></i></a></div>
-									<div style="clear; both"></div>
-								</div>
-								<div class="phone-number">
-									<span>Potrzebujesz pomocy?&nbsp;</span> <span>Zadzwoń! 123 456 789</span>
-								</div>
-								<div style="clear; both"></div>
-							</div>
-
-							<div class="header-right">
-								<div class="account-btn">
-									<ul class="account-menu">
-										<li style="display: none;"><a href="">Witaj $usernname</a></li>
-										<li><a href="register.php" style="border: 2px solid #fc3; border-radius: 5px; font-weight: 600;">ZAREJESTRUJ SIĘ</a></li>
-										<li><a href="login.php">ZALOGUJ SIĘ</a></li>
-									</ul>
-									<div style="clear; both"></div>
-								</div>
-								<div style="clear; both"></div>
-							</div>
-							<div style="clear; both"></div>
-					</div>
-					<div style="clear; both"></div>
-
-					<div class="search-nav">
-						<div class="logo-div"><a href="../index.php"><div class="logo"></div><span style="padding-left: 5px;">&nbsp;BIBLIOTEKA&nbsp;</span><span>ON-LINE</span></a></div>
-						<div class="search-input"><input type="text" placeholder="Wpisz tytuł, nazwę autora, rok wydania lub wydawnictwo"><a href="#"></a></div>
-						<div class="order">order</div>
-					</div>
-					<div style="clear: both"></div>
+					<?php include 'header.php'; ?>
 
 					<div class="menu-div">
 						<ul class="menu">
