bcfb4f561fad472fffd1b202346e91cbb4516e28
shadaxv <aleksandern1116@gmail.com>
2017-02-05 15:06:47 +0100

    Add files via upload

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-  setTimeout(
-    function() {
-      $("#loader-wrapper").fadeOut();
-    }, 2000);
+$(window).on("load", function() {
+    $("#loader-wrapper").fadeOut();
 });
